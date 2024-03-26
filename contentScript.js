055380dcebcc6b824f84943e14e7948647bd6ea8
leonatkdev <leonatkdev@gmail.com>
2024-03-27 00:56:33 +0100

    some late chnages

--- contentScript.js
+++ contentScript.js
@@ -1,3 +1,23 @@
+document.addEventListener('keydown', function(event) {
+  if (event.key === "Escape") {
+      // Remove the canvas
+      const canvas = document.getElementById('colorPickerCanvas');
+      if (canvas) {
+          canvas.remove();
+      }
+      
+      // Remove the zoom lens div
+      const zoomLens = document.getElementById('zoomLens');
+      if (zoomLens) {
+          zoomLens.remove();
+      }
+      
+      // Optionally, remove the event listener for mousemove if you added one for the zoom functionality
+      document.removeEventListener('mousemove', handleMouseMove);
+  }
+});
+
+
 // Injects the canvas and lens into the webpage
 function injectUI() {
   if (document.getElementById("colorPickerCanvas")) return;
@@ -23,6 +43,8 @@ function injectUI() {
   lens.style.overflow = "hidden";
   lens.style.pointerEvents = "none";
   lens.style.zIndex = "100000000";
+  lens.style.backgroundImage = `url("data:image/svg+xml,%3Csvg width='12' height='12' xmlns='http://www.w3.org/2000/svg'%3E%3Crect x='0' y='0' width='12' height='12' fill='none' stroke='black' stroke-width='1'/%3E%3Crect x='1' y='1' width='10' height='10' fill='transparent'/%3E%3C/svg%3E")`;
+  lens.style.backgroundPosition = "center";
   document.body.appendChild(lens);
 
   return { canvas, lens };
@@ -30,38 +52,43 @@ function injectUI() {
 
 // Activates the zoom and color picking functionality
 function activateZoom(dataUrl) {
-  console.log("dataUrl", dataUrl);
   const { canvas, lens } = injectUI();
   const ctx = canvas.getContext("2d", { willreadfrequently: true });
   const img = new Image();
+
 
   img.onload = () => {
     ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
     document.addEventListener("mousemove", (event) => {
       const x = event.clientX;
       const y = event.clientY;
-      const pixel = ctx.getImageData(x, y, 1, 1);
+      const pixel = ctx.getImageData(x, y, 1, 1, {willreadfrequently: true});
       const data = pixel.data;
       const rgba = `rgba(${data[0]}, ${data[1]}, ${data[2]}, ${data[3] / 255})`;
 
       // Update the lens position and background for zoom effect
       lens.style.left = `${x - lens.offsetWidth / 2}px`;
       lens.style.top = `${y - lens.offsetHeight / 2}px`;
-      lens.style.backgroundImage = `url('${dataUrl}')`;
-      lens.style.backgroundSize = `${canvas.width * 2}px ${
-        canvas.height * 2
-      }px`;
-      lens.style.backgroundPosition = `-${x * 2 - lens.offsetWidth / 2}px -${
-        y * 2 - lens.offsetHeight / 2
-      }px`;
+      
+      // The zoomed image as the background
+      const backgroundImage = `url('${dataUrl}')`;
+      const backgroundSize = `${canvas.width * 20}px ${canvas.height * 20}px`;
+      const backgroundPosition = `-${x * 20 - lens.offsetWidth / 20}px -${y * 20 - lens.offsetHeight / 20}px`;
+
+      // SVG grid pattern
+      // const gridPattern = "data:image/svg+xml,%3Csvg width='30' height='30' xmlns='http://www.w3.org/2000/svg'%3E%3Crect width='30' height='30' fill='black'/%3E%3C/svg%3E";
+
+      // Layering the grid pattern over the zoomed background
+      lens.style.backgroundImage = `${backgroundImage}`;
+      lens.style.backgroundSize = `${backgroundSize}, 100px 100px`;
+      lens.style.backgroundPosition = `${backgroundPosition}, 0 0`;
 
       console.log(rgba); // Log or use the RGBA value as needed
     });
   };
-  img.src = dataUrl;
+    img.src = dataUrl;
 }
 
-// activateZoom()
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   console.log("messageContent", message);
--- popup.js
+++ popup.js
@@ -1,36 +1,117 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const headers = document.querySelectorAll('.header');
+document.addEventListener("DOMContentLoaded", () => {
+  const headers = document.querySelectorAll(".header");
 
-    headers.forEach(header => {
-        header.addEventListener('click', () => {
-            const content = header.nextElementSibling;
-            content.style.display = content.style.display === 'flex' ? 'none' : 'flex';
-            header.classList.toggle('active');
-        });
+  headers.forEach((header) => {
+    header.addEventListener("click", () => {
+      const content = header.nextElementSibling;
+      content.style.display =
+        content.style.display === "flex" ? "none" : "flex";
+      header.classList.toggle("active");
     });
+  });
+
+  document.querySelectorAll(".versionBtn").forEach((button) => {
+    button.addEventListener("click", function () {
+      const parentDiv = this.closest('div[style*="flex-direction: column"]');
+      const inputs = parentDiv.querySelectorAll('input[type="text"]');
+      inputs.forEach((input) => input.classList.toggle("showInput"));
+    });
+  });
+
+
+const hex = document.querySelector("#hex");
+
+
+hex.addEventListener("input", function () {
+  const hexValue = hex.value;
+  const rgbaValue = hexToRgba(hexValue);
+  const hslValue = rgbaToHsl(
+    rgbaValue.r,
+    rgbaValue.g,
+    rgbaValue.b,
+    rgbaValue.a
+  );
+  const rgba = `rgba(${rgbaValue.r}, ${rgbaValue.g}, ${rgbaValue.b}, ${rgbaValue.a})`;
+  const hsl = `hsl(${hslValue[0]}, ${hslValue[1]}%, ${hslValue[2]}%, ${hslValue[3]})`;
+
+  document.querySelector("#rgba").value = rgba;
+  document.querySelector("#hsl").value = hsl;
+});
 });
 
-document.getElementById('toggleButton').addEventListener('click', function() {
-    var versionsContainer = document.getElementById('versionsContainer');
-    if (versionsContainer.style.display === "none" || versionsContainer.style.display === "") {
-        versionsContainer.style.display = "flex";
-    } else {
-        versionsContainer.style.display = "none";
+document.getElementById("toggleButton").addEventListener("click", function () {
+  var versionsContainer = document.getElementById("versionsContainer");
+  if (
+    versionsContainer.style.display === "none" ||
+    versionsContainer.style.display === ""
+  ) {
+    versionsContainer.style.display = "flex";
+  } else {
+    versionsContainer.style.display = "none";
+  }
+});
+
+document.getElementById("pickColor").addEventListener("click", function () {
+  console.log("click");
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    // This sends a message to the background script to capture the screen
+    chrome.runtime.sendMessage({ action: "capturePage", tabId: tabs[0].id });
+  });
+});
+
+
+function hexToRgba(hex) {
+    const hexValue = hex.replace("#", "");
+    const r = parseInt(hexValue.substring(0, 2), 16);
+    const g = parseInt(hexValue.substring(2, 4), 16);
+    const b = parseInt(hexValue.substring(4, 6), 16);
+    const a = parseInt(hexValue.substring(6, 8), 16) / 255 || 1;
+    
+    return { r, g, b, a };
+}
+
+function rgbaToHex(r, g, b, a) {
+  return (
+    "#" +
+    [r, g, b]
+      .map((x) => {
+        const hex = x.toString(16);
+        return hex.length === 1 ? "0" + hex : hex;
+      })
+      .join("") +
+    Math.round(a * 255)
+      .toString(16)
+      .padStart(2, "0")
+  );
+}
+
+function rgbaToHsl(r, g, b, a) {
+  (r /= 255), (g /= 255), (b /= 255);
+  const max = Math.max(r, g, b),
+    min = Math.min(r, g, b);
+  let h,
+    s,
+    l = (max + min) / 2;
+
+  if (max === min) {
+    h = s = 0; // achromatic
+  } else {
+    const d = max - min;
+    s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+    switch (max) {
+      case r:
+        h = (g - b) / d + (g < b ? 6 : 0);
+        break;
+      case g:
+        h = (b - r) / d + 2;
+        break;
+      case b:
+        h = (r - g) / d + 4;
+        break;
     }
-});
+    h /= 6;
+  }
 
+  return [h * 360, s * 100, l * 100, a];
+}
 
-// document.getElementById('pickColor').addEventListener('click', function() {
-//     console.log('clicked')
-//     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-//       chrome.tabs.sendMessage(tabs[0].id, {action: "activateZoom"});
-//     });
-//   });
-
-
-document.getElementById('pickColor').addEventListener('click', function() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        // This sends a message to the background script to capture the screen
-        chrome.runtime.sendMessage({action: "capturePage", tabId: tabs[0].id});
-    });
-});
